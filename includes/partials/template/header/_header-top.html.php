9e2369d35cb8eafe1ca37fa08e6e545d014100fb
Leonardo Pang <leonardopang12@gmail.com>
2026-07-10 14:05:23 -0300

    Refactor typography and spacing across various components
    - Updated font sizes for titles and descriptions in _about.scss, _testimonial.scss, _hero.scss, _benefits.scss, _category-slider.scss, _featured-products.scss, _product-grid.scss, and _single-product.scss to improve visual hierarchy.
    - Adjusted padding values in _header.scss, _featured-products.scss, _content.scss, and _related.scss for better layout consistency and responsiveness.

--- includes/partials/template/header/_header-top.html.php
+++ includes/partials/template/header/_header-top.html.php
@@ -1,6 +1,6 @@
 <?php
 $module  = get_field('header', 'tema');
-$top_text = $module['top_text'] ?? __('REFEIÇÕES CONGELADAS &bull; ENTREGAMOS EM ATÉ 23 KM DA VILA PRUDENTE-SP &bull; TAXA GRÁTIS ACIMA DE R$600,00', 'lucci-fresh');
+$top_text = wp_is_mobile() ? $module['top']['mobile'] : $module['top']['text'];
 ?>
 <div class="o-header__top">
   <div class="s-container">
