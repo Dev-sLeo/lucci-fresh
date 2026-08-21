b5604ce5c6201afb8f3cd307187ed807dd0cf558
Leonardo Pang <leonardopang12@gmail.com>
2026-08-21 09:48:41 -0300

    feat: make CPF field required in checkout form and remove label asterisk

--- extension/woocommerce.php
+++ extension/woocommerce.php
@@ -227,10 +227,6 @@ add_filter('woocommerce_checkout_fields', function ($fields) {
     if (isset($fields['billing']['billing_cpf'])) {
         $fields['billing']['billing_cpf']['class']    = ['form-row-first'];
         $fields['billing']['billing_cpf']['required'] = true;
-        $label                                        = isset($fields['billing']['billing_cpf']['label']) ? $fields['billing']['billing_cpf']['label'] : __('CPF', 'arterra');
-        if (! preg_match('/\*\s*$/', $label)) {
-            $fields['billing']['billing_cpf']['label'] = $label . ' *';
-        }
     }
     if (isset($fields['order']['order_comments'])) {
         $fields['order']['order_comments']['label'] = __('Observações para entrega', 'arterra');
